7ff9541901b8983155ee3a10fa5955236bb922a9
angelica <angelica@experimentality.com>
2020-05-02 19:16:13 -0500

    punto 2 resuelto

---
+++ punto_2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include<time.h>
+#include <pthread.h>
+
+void *factorial (void *argumento);
+// void *createThreads (void *argumento);
+
+int main (int argc, char *argv[]) {
+  srand(time(0));
+  pthread_t threads[argc];
+  if (argc <= 1) {
+    printf ("Debes indicar al menos un entero.\n");
+    exit(0);
+  }
+  for (int i = 1; i < argc; i++){
+    printf ("indice %d, arg %s\n", i, argv[i]);
+    pthread_create (&threads[i], NULL, factorial,(void *) argv[i]);
+  }
+  for (int i = 1; i < argc; i++){
+     pthread_join (threads[i], NULL);
+     printf ("Fin del hilo: %d\n", i);
+  }
+  return 0;
+}
+
+// void *createThreads (void *argumento){
+//   pthread_t h1;
+//   pthread_create (&h1, NULL, factorial, (void *) argumento);
+//   printf("In function \nthread id = %d\n", pthread_self()); 
+//   pthread_join (h1, NULL);
+// }
+
+void *factorial (void *argumento) {
+  printf("In function \nthread id = %d\n", pthread_self()); 
+  long long int resultado= 1;
+  int num, n = atoi((char *) argumento);
+  for (num= 2; num<= n; num++) {
+    resultado= resultado*num;
+    printf ("Factorial de %d, resultado parcial %lld\n", n, resultado);
+    sleep (random()%3);
+  }
+  printf ("El factorial de %d es %lld\n", n, resultado);
+  pthread_exit(NULL);
+}
